3b97bf9d8f40d11e011209e91583974c8495ad8e
Alumne Matí <alumne@localhost.localdomain>
2025-09-18 10:53:38 +0200

    display count

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,48 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+  private Rigidbody rb;
+  private float movementX; //movimiento en x
+  private float movementY; //movimiento en y 
 
-    }
-    
-     void OnMove (InputValue movementValue)
-   {
+  public float speed = 0; //velocidad
+
+  private int count;//contador
+
+
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    count = 0; //inicializado a cero
+  }
+
+  void OnMove(InputValue movementValue)
+  {
+    Vector2 movementVector = movementValue.Get<Vector2>();
+    movementX = movementVector.x;
+    movementY = movementVector.y;
+  }
+
+  private void FixedUpdate()
+  {
+    Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+    rb.AddForce(movement * speed); //velocidad de movimiento
+
+  }
    
+    void OnTriggerEnter(Collider other) 
+   {
+    if (other.gameObject.CompareTag("PickUp")) //Comprueba que el objeto chocado es el llamaod pickup
+    {
+      other.gameObject.SetActive(false); //Desactiava el objeto
+      count += 1;
+       }
+     
    }
 
 
