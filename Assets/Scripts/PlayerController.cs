95dfb9f964e6c125cb9ba542f62f736b064808f5
Alumne Matí <alumne@localhost.localdomain>
2025-09-18 08:52:11 +0200

    proyecto unity 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    
+     void OnMove (InputValue movementValue)
+   {
+   
+   }
+
+
+}
